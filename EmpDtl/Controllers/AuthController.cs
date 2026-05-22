c4eec3cbd017b943700246475aa06a8412bb932f
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-05-22 16:19:06 +0530

    Add token generate

--- EmpDtl/Controllers/AuthController.cs
+++ EmpDtl/Controllers/AuthController.cs
@@ -1,5 +1,10 @@
 ﻿using EmpDtl.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace EmpDtl.Controllers
 {
@@ -19,7 +24,37 @@ namespace EmpDtl.Controllers
 
         private string Generatetoken(User user)
         {
-            
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name,user.Username),
+
+                new Claim(ClaimTypes.Role,user.Role),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                    _configuration["JWT.key"])
+                );
+
+            var cred = new SigningCredentials(
+                key,SecurityAlgorithms.HmacSha256
+                );
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT.Issuer"],
+                audience: _configuration["JWT.Audience"],
+                claims:claims,
+                expires:DateTime.Now.AddMinutes(
+                    Convert.ToDouble(
+                        _configuration["JWT.Expair"]
+                        )
+                    ),
+                signingCredentials:cred
+                
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
 
