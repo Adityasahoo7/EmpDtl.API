493e2a4d86708b10f78959c4fe280d58efc5e645
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-06-13 12:13:52 +0530

    Add auth

--- EmpDtl/obj/Debug/net8.0/EmpDtl.AssemblyInfo.cs
+++ EmpDtl/obj/Debug/net8.0/EmpDtl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpDtl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56fc0a92da3b2abb4ab8b11af53657d7b24f64a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ca6ea67925834a8f9f5434e6cdddc8d21833c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpDtl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpDtl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
