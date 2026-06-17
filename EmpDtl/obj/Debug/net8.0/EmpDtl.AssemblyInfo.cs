9ad3ff1766375ffd4867e023ce176e44f9eb4506
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-06-17 11:37:06 +0530

    Add log v1

--- EmpDtl/obj/Debug/net8.0/EmpDtl.AssemblyInfo.cs
+++ EmpDtl/obj/Debug/net8.0/EmpDtl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpDtl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8900614ccbfc327432a8366a160c450d9edc62b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01655f4927082462318c546ce3488a33b9de3539")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpDtl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpDtl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
