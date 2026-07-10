15be5561461197dba84721ee8ea089e2b441ee64
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-07-10 22:41:39 +0530

    Add changepassword controller

--- EmpDtl/obj/Debug/net8.0/EmpDtl.AssemblyInfo.cs
+++ EmpDtl/obj/Debug/net8.0/EmpDtl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpDtl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a10026cec9e51003281407170b9379c2186b328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b05c636a113a0fc774494172a68922db0e275ea4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpDtl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpDtl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
